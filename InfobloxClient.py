6465cac0372e22ece85950981d7ab4c661ffb947
jakson@jakson.dev <jwainwright@GPAdmins-MacBook-Pro.local>
2020-06-10 16:59:21 -0600

    Added ability to update extensible attributes values. Updated README

--- InfobloxClient.py
+++ InfobloxClient.py
@@ -9,30 +9,58 @@ class InfobloxClient:
         self.infoblox_user = infoblox_vars.infoblox_user
         self.infoblox_password = infoblox_vars.infoblox_pass
         self.url_ip_block = infoblox_vars.infoblox_url_ip_block
+        self.headers = {"Content_Type": "application/json"}
 
     def send_get_request(self, uri, *args):
-        headers = {"Content_Type": "application/json"}
         if args:
-            request = requests.get(f'{self.infoblox_base_url}{uri}', headers=headers, verify=False,
+            request = requests.get(f'{self.infoblox_base_url}{uri}', headers=self.headers, verify=False,
                                    auth=HTTPBasicAuth(infoblox_vars.infoblox_user,
                                                       infoblox_vars.infoblox_pass), data=args[0])
         else:
-            request = requests.get(f'{self.infoblox_base_url}{uri}', verify=False,
+            request = requests.get(f'{self.infoblox_base_url}{uri}', headers=self.headers, verify=False,
                                    auth=HTTPBasicAuth(infoblox_vars.infoblox_user,
                                                       infoblox_vars.infoblox_pass))
         response = json.loads(request.text.strip('[]'))
         return response
 
-    def get_ext_attrs(self, attr_name):
+    def send_put_request(self, uri, data):
+        request = requests.put(f'{self.infoblox_base_url}{uri}', headers=self.headers, verify=False,
+                               auth=HTTPBasicAuth(infoblox_vars.infoblox_user,
+                                                  infoblox_vars.infoblox_pass), data=data)
+        return request.text
+
+    def get_ext_attr(self, attr_name):
         ext_atrr_values = []
         data = json.dumps({"name": attr_name})
         request = self.send_get_request("extensibleattributedef?_return_fields=list_values", data)
+        ext_atrr_values.append(request['_ref'])
         for list_key in request['list_values']:
             ext_atrr_values.append(list_key['value'])
         return ext_atrr_values
 
-    def set_ext_attrs(self, attrs):
-        return 'To do '
+    def return_ext_attr_values(self, attr_name):
+        return self.get_ext_attr(attr_name)[1:]
+
+    def return_ext_attr_ref(self, attr_name):
+        return self.get_ext_attr(attr_name)[0]
+
+    def add_ext_attr_values(self, attr_name, *args):
+        put_dict = {
+            "name": attr_name,
+            "type": "ENUM",
+            "list_values":[
+
+            ]
+
+        }
+        for value in self.return_ext_attr_values(attr_name):
+            put_dict["list_values"].append({"value": value})
+        for arg in args:
+            if {"value": arg} not in put_dict["list_values"]:
+                put_dict["list_values"].append({"value": arg})
+        put_dict = str(put_dict).replace("\'", "\"")
+        uri = self.return_ext_attr_ref(attr_name)
+        return self.send_put_request(uri, put_dict)
 
     def get_next_ip(self):
         return 'To do'
@@ -53,4 +81,4 @@ class InfobloxClient:
         return 'To do'
 
     def test_func(self):
-        print(self.get_ext_attrs('Program Name'))
+        print(self.add_ext_attr_values('Program Name', 'test5', 'test7', 'test4' ))
