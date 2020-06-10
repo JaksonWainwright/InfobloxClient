2c1dfc19306e9c6e94c89741bb7e6fd5fe1d404b
jakson@jakson.dev <jwainwright@GPAdmins-MacBook-Pro.local>
2020-06-10 15:44:03 -0600

    finished writing the functions for getting extensible attributes

---
+++ MainTesting.py
@@ -0,0 +1,5 @@
+import InfobloxConnector
+
+infoblox_connection = InfobloxConnector.InfobloxConnector()
+
+infoblox_connection.test_func()
--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@ from secure_conf import infoblox_credentials
 
 class infoblox_vars:
     infoblox_wapi_version = 'v2.7.3'
-    infoblox_base_url = f'https://192.168.0.2/wapi/{infoblox_wapi_version}/networkview?'
+    infoblox_base_url = f'https://192.168.0.2/wapi/{infoblox_wapi_version}/'
     infoblox_user = infoblox_credentials.infoblox_user
     infoblox_pass = infoblox_credentials.infoblox_pass
     infoblox_url_ip_block = ipaddress.IPv4Network('199.73.119.0/24')
